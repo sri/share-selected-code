c1a64ec4aed3662440253d144c850fe9e989d372
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2018-08-19 08:24:34 -0700

    Rename to imporve readability

--- src/sites.ts
+++ src/sites.ts
@@ -1,23 +1,23 @@
 'use strict';
 
-interface Format {
+interface Formatter {
   formatBold(text: string): string;
   formatSelection(text: string): string;
 }
 
-class JiraFormat implements Format {
+class JiraFormatter implements Formatter {
   formatBold(text: string) { return `*${text}*`; }
   formatSelection(text: string) { return `{noformat}\n${text}\n{noformat}`; }
 }
 
-class SlackFormat implements Format {
+class SlackFormatter implements Formatter {
   formatBold(text: string) { return `*${text}*`; }
   formatSelection(text: string) { return '```\n' + text + '\n```'; }
 }
 
-const SITES: { [index: string]: Format } = {
-  jira: new JiraFormat(),
-  slack: new SlackFormat(),
+const SITES: { [index: string]: Formatter } = {
+  jira: new JiraFormatter(),
+  slack: new SlackFormatter(),
 };
 
 export default SITES;
